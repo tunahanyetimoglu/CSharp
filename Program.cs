8a15a7f2bad015835d28c880753bc02368390ae8
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-03 11:56:17 +0300

    ilk program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birinci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b;
+            double c = 5.0;
+            int d = a++;
+            int e = ++a;
+            int f = Convert.ToInt32(Math.Pow(3, 4));
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A değişkeni ={0} , b degiskeni ={1} , c degiskeni ={2}",a,b,c); //Ekrana Double basmıyor!!
+            Console.WriteLine("A değişkeni =" + a + ", b degiskeni =" + b + " ,c degiskeni =" + c);    // Format yazımlarına bak!
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("e = " + e);
+            Console.WriteLine("f = " + f);
+            Console.ReadKey(); 
+        }
+    }
+}
