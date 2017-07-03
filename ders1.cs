65831ccc9431f34eedd99ce4db20eb2a1960bb46
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-03 13:08:01 +0300

    Update and rename Program.cs to ders1.cs

--- ders1.cs
+++ ders1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace birinci
+namespace CSharp
 {
-    class Program
+    class ders1
     {
         static void Main(string[] args)
         {
