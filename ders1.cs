c5792ee0deaf698ee355e081252666379f6093a7
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-03 13:19:14 +0300

    Update ders1.cs

--- ders1.cs
+++ ders1.cs
@@ -17,7 +17,7 @@ namespace CSharp
             int e = ++a;
             int f = Convert.ToInt32(Math.Pow(3, 4));
             b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("A değişkeni ={0} , b degiskeni ={1} , c degiskeni ={2}",a,b,c); //Ekrana Double basmıyor!!
+            Console.WriteLine("A değişkeni ={0} , b degiskeni ={1} , c degiskeni ={2:0.0}",a,b,c);
             Console.WriteLine("A değişkeni =" + a + ", b degiskeni =" + b + " ,c degiskeni =" + c);    // Format yazımlarına bak!
             Console.WriteLine("d = " + d);
             Console.WriteLine("e = " + e);
