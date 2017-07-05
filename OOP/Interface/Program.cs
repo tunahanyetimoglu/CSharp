146d969908e26f19b0b86b1873ba04dc795201d9
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-05 15:46:19 +0300

    Create Program.cs

---
+++ OOP/Interface/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class c = new Class();
+
+            c.metod1();
+            c.metod2();
+
+            Console.ReadKey();
+        }
+    }
+}
