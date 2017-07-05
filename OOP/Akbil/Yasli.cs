5a19e4981971d374af1be4b7eebbfe1142b036ad
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-06 01:41:35 +0300

    Create Yasli.cs

---
+++ OOP/Akbil/Yasli.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akbil
+{
+    class Yasli:Class1
+    {
+        public Yasli(int abone_id, string isim, string soyisim, int yas, double kredi):base(abone_id,isim,soyisim,yas,kredi)
+        {
+
+        }
+
+        public override void krediGuncelle()
+        {
+            if (Yas < 30)
+            {
+                Kredi = Kredi - 1;
+            }
+            else if (30 <= Yas && Yas < 60)
+            {
+                Kredi = Kredi - 2;
+            }
+            else
+            {
+                Kredi = Kredi - 0.5;
+            }
+        }
+    }
+}
