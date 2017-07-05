78f817a5ce2373684914b2b352f10e799ef8c600
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-05 15:33:00 +0300

    Add files via upload

---
+++ OOP/Kalitim2/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+     abstract class Shape
+    {
+        protected int boy;
+        protected int en;
+
+        abstract public int En { set; get; }
+        abstract public int Boy { set; get; }
+
+        public Shape(int boy, int en)
+        {
+            this.boy = boy;
+            this.en = en;
+        }
+
+        abstract public int Alan();
+        abstract public void EkranaYaz();
+
+    }
+}
---
+++ OOP/Kalitim2/Ucgen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Ucgen : Shape
+    {
+        private string tip;
+        public Ucgen(string tip,int boy,int en) : base(boy, en)
+        {
+            this.tip = tip;
+        }
+
+        override public int Boy
+        {
+            get
+            {
+                return boy;
+            }
+            set
+            {
+                Boy = value;    
+            }
+        }
+        override public int En
+        {
+            get
+            {
+                return en;
+            }
+            set
+            {
+                Boy = value;              
+            }
+        }
+        public override int Alan()
+        {
+            return en * boy / 2;
+        }
+        public override void EkranaYaz()
+        {
+            Console.WriteLine("En = " + en);
+            Console.WriteLine("Boy = " + boy);
+            Console.WriteLine("Tip : = " + this.tip);
+            Console.WriteLine("Alan = " + Alan());
+            Console.WriteLine();
+        }
+    }
+}
