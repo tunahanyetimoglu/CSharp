c3804f39f6ebd81c9466a433b59e6c13cf6a5498
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-05 15:46:08 +0300

    Create IArayuz.cs

---
+++ OOP/Interface/IArayuz.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    interface IArayuz
+    {
+        void metod1();
+        int metod2();
+
+    }
+}
