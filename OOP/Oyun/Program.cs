8435ef49ea71a44ad980a1f2f1c8d11e48c35e12
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-05 10:35:32 +0300

    Create Program.cs

---
+++ OOP/Oyun/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oyun game = new Oyun(1,100);
+        //  Oyun game = new Oyun(100);
+        
+            game.basla();
+
+
+        }
+    }             
+}
