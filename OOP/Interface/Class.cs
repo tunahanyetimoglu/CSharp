2b530d88d9b6a7924816231036ddb649ad424241
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-05 15:46:34 +0300

    Create Class.cs

---
+++ OOP/Interface/Class.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Class : IArayuz
+    {
+        public void metod1()
+        {
+            Console.WriteLine("Metod1");
+        }
+
+        public int metod2()
+        {
+            Console.WriteLine("Metod2");
+
+            return 1;
+        }
+    }
+
+
+}
