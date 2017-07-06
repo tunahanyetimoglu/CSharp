2e9e8e1fa9cf5d857f88b1ec3e77444d68a9ed07
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-06 10:03:51 +0300

    Update ogrenci.cs

--- OOP/Akbil/ogrenci.cs
+++ OOP/Akbil/ogrenci.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,7 @@ namespace Akbil
             }
             else if (30 <= Yas && Yas < 60)
             {
+                
                 Kredi = Kredi - 2;
             }
             else
