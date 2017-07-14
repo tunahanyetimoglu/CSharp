3cc74c8ef8147e1187eb2ae29e638a2060e85aa1
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-07-14 16:06:02 +0300

    Add files via upload

---
+++ SQL/Kutuphane/ENTITYLAYER/EKATEGORI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITYLAYER
+{
+    public class EKATEGORI : IDisposable
+    {
+        int _ID;
+        string _ADI;
+
+        public int ID
+        {
+            get
+            {
+                return _ID;
+            }
+
+            set
+            {
+                _ID = value;
+            }
+        }
+
+        public string ADI
+        {
+            get
+            {
+                return _ADI;
+            }
+
+            set
+            {
+                _ADI = value;
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
